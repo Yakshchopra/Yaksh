660be29b45f932999700562fca4eae1fafe0731d
Yakshchopra <yakshchopra@gmail.com>
2020-06-01 20:44:02 +0530

    landing

---
+++ app.js
@@ -0,0 +1,50 @@
+AOS.init();
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('about'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './res/Growth Animation.json'
+  });
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('IoT'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './res/Testig Tech Colored.json'
+    });
+
+    var animation = bodymovin.loadAnimation({
+      container: document.getElementById('IoT-2'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: './res/Testig Tech Colored.json'
+      });
+  
+
+    var animation = bodymovin.loadAnimation({
+      container: document.getElementById('web'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: './res/Blogging Colored.json'
+      });
+
+      var animation = bodymovin.loadAnimation({
+        container: document.getElementById('works'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './res/Build Product Colored.json'
+        });
+  
+        var animation = bodymovin.loadAnimation({
+          container: document.getElementById('works-2'),
+          renderer: 'svg',
+          loop: true,
+          autoplay: true,
+          path: './res/Build Product Colored.json'
+          });
